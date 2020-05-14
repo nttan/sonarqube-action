617abeddd615045b79881f036e638a25c062b626
Nguyen Tan <nttan@users.noreply.github.com>
2020-05-14 19:35:36 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -6,7 +6,7 @@ exec 3>&1 # make stdout available as fd 3 for the result
 exec 1>&2 # redirect all output to stderr for logging
 
 #root=$(dirname "${0}" | while read -r a; do cd "${a}" && pwd && break; done)
-root="."
+root=""
 export root
 source "/common.sh"
 scanner_report_file="${GITHUB_WORKSPACE}/.scannerwork/report-task.txt"
